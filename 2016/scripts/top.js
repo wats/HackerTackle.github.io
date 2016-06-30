939a157e99f435308c1bfe2c512af659cfd280dd
kenz_firespeed <firespeedkenz@gmail.com>
2016-06-30 13:12:14 +0900

    正弦波をオブジェクトで管理して複数の波形を同時表示できるように修正

--- 2016/scripts/top.js
+++ 2016/scripts/top.js
@@ -3,61 +3,84 @@ const WIDTH = 2000;
 const HEIGHT = 600;
 const STEP = 20;
 const CENTER_X = WIDTH / 2;
+const CENTER_Y = HEIGHT / 2;
+const WAVE_HEIGHT = HEIGHT / 3;
 
 var canvas = document.getElementById('header_image');
+
+var pointArray = [new Point()];
+for(i=0; i<3; i++){
+  pointArray.push(new Point());
+}
+
 if (canvas.getContext) {
     var context = canvas.getContext('2d');
     context.lineWidth=1;
     setInterval(anim,16);
 }
-var i = 0;
-var waveHeight = 0;
-var prey = Math.cos((i)/100) * HEIGHT + 300;
-var prey2 = Math.sin((i)/100) * HEIGHT + 300;
-
-function randomValue(){
-    return Math.floor(Math.random() * 255);
-}
-function createRandomColor(){
-    return 'rgb('
-      + randomValue() + ' ,' +
-      + randomValue() + ' ,' +
-      + randomValue()
-    ')';
-}
-function feedOut(){
-    context.globalAlpha = 0.1;
-    context.fillStyle = "rgb(0, 29, 33)";
-    context.fillRect(0, 0, WIDTH, HEIGHT);
-
-}
-var sinColor;
 function anim(){
-  x = i * STEP % WIDTH;
-  if(x==0){
-    waveHeight = Math.random() * 300;
-    sinColor = createRandomColor();
+  for (point of pointArray) {
+    move(point);
+    paint(context, point);
   }
-  var color;
-  if( x < CENTER_X){
-    color = 1;
-  }else{
-    color = (1 - (x / WIDTH))*2;
-  }
-  var y = Math.cos((i*10)/100) * waveHeight + 300;
-  var y2 = Math.sin((i*10)/100) * waveHeight + 300;
-  context.globalAlpha = color;
-
-  context.strokeStyle = sinColor;
-  context.beginPath();
-  context.moveTo(x -STEP , prey);
-  context.lineTo(x , y);
-  context.stroke();
-  context.beginPath();
-  context.moveTo(x -STEP , prey2);
-  context.lineTo(x , y2);
-  context.stroke();
-  prey = y;
-  prey2 = y2;
-  i++;
+}
+ function randomValue(){
+     return Math.floor(Math.random() * 255);
  }
+ function createRandomColor(){
+     return 'rgb('
+       + randomValue() + ' ,' +
+       + randomValue() + ' ,' +
+       + randomValue()
+     ')';
+ }
+ function Point(){
+   reset(this);
+ }
+ function move(point, time){
+   point.preX = point.x;
+   point.preY = point.y;
+   point.wave += point.waveWidth;
+   if(point.x > WIDTH){
+     reset(point);
+   }else{
+     point.x += point.speed;
+     point.y =  convertY(point);
+   }
+ }
+ function getAlpha(point){
+   if( point.x < CENTER_X){
+     return 1;
+   }else{
+     return (1 - (point.x / WIDTH))*2;
+   }
+ }
+ function reset(point){
+     point.waveHeight = Math.random() * WAVE_HEIGHT + 10;
+     point.speed = Math.random() * 10 + 5;
+     point.waveWidth = Math.random() / 5;
+     point.wave = Math.random();
+
+     point.preY =  convertY(point);
+     point.wave += point.waveWidth;
+     point.y =  convertY(point);
+     point.x = point.speed;
+     point.preX = 0;
+     point.color = createRandomColor();
+
+ }
+
+function convertY(point){
+  return Math.cos(point.wave) * point.waveHeight + CENTER_Y;
+}
+function paint(context, point){
+  context.globalAlpha = getAlpha(point);
+  context.strokeStyle = point.color;
+  context.beginPath();
+  context.moveTo(point.preX , point.preY);
+  context.lineTo(point.x , point.y);
+  context.stroke();
+  context.globalAlpha = 0.2;
+  context.fillStyle = "rgb(0, 29, 33)";
+  context.fillRect(point.preX - 1, 0, point.speed, HEIGHT);
+}
